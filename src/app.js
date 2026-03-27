6e335d20226173de4f41fb903ff0b97451959b76
jo-oseeph <realsejjo2001@gmail.com>
2026-03-27 22:47:11 +0300

    order routes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import authRoutes from "./routes/authRoutes.js";
 import productRoute from "./routes/productRoute.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import cartRoutes from "./routes/cartRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoute);
 app.use(errorHandler);
